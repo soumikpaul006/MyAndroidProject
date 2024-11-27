cd96b41fe59689430103d5e650cc87193b2b634c
Soumik Paul <prick5036@gmail.com>
2024-11-27 10:44:30 -0500

    edit of text view in main activity is added

--- app/src/main/java/com/codegalaxy/mock21nov/view/FragmentActivity.kt
+++ app/src/main/java/com/codegalaxy/mock21nov/view/FragmentActivity.kt
@@ -2,12 +2,15 @@ package com.codegalaxy.mock21nov.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.codegalaxy.mock21nov.FragmentAdapter
 import com.codegalaxy.mock21nov.R
 import com.codegalaxy.mock21nov.databinding.ActivityFragmentBinding
+import com.codegalaxy.mock21nov.viewmodel.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -16,6 +19,7 @@ class FragmentActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityFragmentBinding
     private lateinit var adapter: FragmentAdapter
+    private val sharedViewModel:SharedViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +28,10 @@ class FragmentActivity : AppCompatActivity() {
 
         binding = ActivityFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sharedViewModel.sharedData.observe(this) {data->
+            binding.textView.text=data
+        }
 
         adapter = FragmentAdapter(this)
         binding.viewPager.adapter = adapter
